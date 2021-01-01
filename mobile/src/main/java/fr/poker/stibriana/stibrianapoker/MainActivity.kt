a284d589bb66e4d07684b381248c003cfaf8a77e
Brian Giannini <brian.giannini.1@gmail.com>
2021-01-01 23:31:55 +0100

    upgrade app to 2020 code

--- mobile/src/main/java/fr/poker/stibriana/stibrianapoker/MainActivity.kt
+++ mobile/src/main/java/fr/poker/stibriana/stibrianapoker/MainActivity.kt
@@ -9,9 +9,11 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
 
+const val LINK_SCORE_SHEET = "https://docs.google.com/spreadsheets/d/1hdUt6IjVTEQW_RgmaxluXW4IRBAMucE0MBxc-ypa9pg/edit?usp=sharing"
 
 class MainActivity : Activity() {
 
@@ -19,31 +21,38 @@ class MainActivity : Activity() {
     private var gain: Int = 0
     private var coef = doubleArrayOf(0.0, 5.0, 4.0, 3.0, 2.5, 2.25, 2.0, 1.75, 1.5, 1.25)
     private var coef2 = doubleArrayOf(0.0, 3.5, 2.0, 7 / 6.5)
-    internal var joueurs = 0
-    internal var place = 0
+    private var playersNumber = 0
+    private var placeNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val joueursRecup = findViewById<View>(R.id.joueurs) as EditText
-        val placeRecup = findViewById<View>(R.id.place) as EditText
+        val players = findViewById<View>(R.id.players) as EditText
+        val place = findViewById<View>(R.id.place_editext) as EditText
 
-        joueursRecup.setOnKeyListener { view, actionId, event ->
+        players.setOnKeyListener { _, actionId, event ->
             if (KeyEvent.KEYCODE_ENTER == actionId) {
                 sending()
             }
             false
         }
 
-        placeRecup.setOnKeyListener { view, actionId, event ->
+        place.setOnKeyListener { _, actionId, _ ->
             if (KeyEvent.KEYCODE_ENTER == actionId) {
                 sending()
             }
             false
+        }
+
+        send_result.setOnClickListener {
+            sending()
+        }
+
+        goToScore.setOnClickListener {
+            goScore()
         }
     }
-
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -57,58 +66,48 @@ class MainActivity : Activity() {
         // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
 
-
         return if (id == R.id.action_settings) {
             true
         } else super.onOptionsItemSelected(item)
 
     }
 
-    fun sendMessage(view: View) {
-        sending()
-    }
-
-    fun sending() {
-        val joueursRecup = findViewById<View>(R.id.joueurs) as EditText
-        val placeRecup = findViewById<View>(R.id.place) as EditText
-        val pointsdisplay = findViewById<View>(R.id.textView) as TextView
-        val gaindisplay = findViewById<View>(R.id.textView2) as TextView
-
-        if (joueursRecup.text.toString().trim { it <= ' ' }.isEmpty()) {
-            Toast.makeText(this, "Veuillez entrez le nombre de joueurs", Toast.LENGTH_SHORT).show()
-        } else if (placeRecup.text.toString().trim { it <= ' ' }.isEmpty()) {
-            Toast.makeText(this, "Veuillez entrez votre place", Toast.LENGTH_SHORT).show()
+    private fun sending() {
+        if (players.text.toString().trim { it <= ' ' }.isEmpty()) {
+            Toast.makeText(this, getString(R.string.enter_players_number_text), Toast.LENGTH_SHORT).show()
+        } else if (place_editext.text.toString().trim { it <= ' ' }.isEmpty()) {
+            Toast.makeText(this, getString(R.string.enter_place_text), Toast.LENGTH_SHORT).show()
         } else {
 
-            joueurs = Integer.valueOf(joueursRecup.text.toString())
-            place = Integer.valueOf(placeRecup.text.toString())
+            playersNumber = Integer.valueOf(players.text.toString())
+            placeNumber = Integer.valueOf(place_editext.text.toString())
 
-            if (place > joueurs) {
-                Toast.makeText(this, "Vous avez rentré de mauvaises informations", Toast.LENGTH_SHORT).show()
+            if (placeNumber > playersNumber) {
+                Toast.makeText(this, getString(R.string.wrong_informaiton_message_text), Toast.LENGTH_SHORT).show()
             } else {
 
-                if (place > 9) {
-                    points = Math.round((joueurs + 1 - place).toFloat())
+                points = if (placeNumber > 9) {
+                    (playersNumber + 1 - placeNumber).toFloat().roundToInt()
                 } else {
-                    points = Math.round(coef[place] * (joueurs - place + 1)).toInt()
+                    (coef[placeNumber] * (playersNumber - placeNumber + 1)).roundToInt()
                 }
 
-                if (place < 4) {
-                    gain = Math.round(joueurs * coef2[place]).toInt()
-                } else if (place == 4) {
-                    gain = joueurs * 7 - Math.round(joueurs * 3.5).toInt() - Math.round((joueurs * 2).toFloat()) - Math.round(joueurs * 7 / 6.5).toInt()
+                gain = if (placeNumber < 4) {
+                    (playersNumber * coef2[placeNumber]).roundToInt()
+                } else if (placeNumber == 4) {
+                    playersNumber * 7 - (playersNumber * 3.5).roundToInt() - (playersNumber * 2).toFloat().roundToInt() - (playersNumber * 7 / 6.5).roundToInt()
                 } else {
-                    gain = 0
+                    0
                 }
 
-                pointsdisplay.text = points.toString() + "pts"
-                gaindisplay.text = gain.toString() + "€"
+                points_txt.text = getString(R.string.showed_pts, points)
+                gain_txt.text = getString(R.string.showed_gain, gain)
             }
         }
     }
 
-    fun goScore(view: View) {
-        val uri = Uri.parse("https://docs.google.com/spreadsheets/d/1w6YO3aF3N5l0KGuf-y67v2m2EpgKSJn2Z97I4tpZFnQ/edit?usp=sharing")
+    private fun goScore() {
+        val uri = Uri.parse(LINK_SCORE_SHEET)
         val intent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(intent)
     }
